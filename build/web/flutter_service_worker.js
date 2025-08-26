a6eca961aa026aed567d11803f90bae9c7dfbbde
Adryc Allen Catapang <adrycallencatapang@Adrycs-MacBook-Air.local>
2025-08-26 21:06:53 +0800

    fix: map location

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "18bd1d86b659a9ceb8320f13770f58da",
-"version.json": "73af2adfc7b8b0ece7c231d8706774e0",
-"index.html": "4815be5c3e0e55af4ff1cad77eecd89f",
-"/": "4815be5c3e0e55af4ff1cad77eecd89f",
-"main.dart.js": "cc9b74e22bf3bc83460d9bcd70627bd9",
+const RESOURCES = {"flutter_bootstrap.js": "7b0971eecf53b03a41441bd65e41c6b3",
+"version.json": "c1fca7123e552985ccf1b489d2f61287",
+"index.html": "418f1ab348d8676228094a477530e98c",
+"/": "418f1ab348d8676228094a477530e98c",
+"main.dart.js": "04e664e13517aef71f634bd184a59acb",
 "flutter.js": "f31737fb005cd3a3c6bd9355efd33061",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
