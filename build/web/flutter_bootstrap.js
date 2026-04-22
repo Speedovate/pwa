a94a5559725394a32569a5260fb8b69a835ada0d
Adryc Allen Catapang <catapangadrycallen@gmail.com>
2026-04-22 16:41:54 +0800

    hotfix: splash loop

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48ab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=1.0.35+55"},{}]};
+_flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48ab","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=1.0.30+50"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "507107183" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1875687813" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
